c8ffda4f42c625ae2cc2aad8aa749ae7758597d2
Ambar Bose <ambar.bs@gmail.com>
2020-03-29 15:44:46 +1100

    Updates

---
+++ precache-manifest.553b6432d188b51c324ad1697751cb58.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c320d5135a9f422c72e0ad5c51a92500",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03b68f104338a29185b5",
+    "url": "/static/css/main.8917f35e.chunk.css"
+  },
+  {
+    "revision": "d7920dedff654b6663bb",
+    "url": "/static/js/2.8da34373.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.8da34373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03b68f104338a29185b5",
+    "url": "/static/js/main.f3f3e4af.chunk.js"
+  },
+  {
+    "revision": "4dad5698413e88698df0",
+    "url": "/static/js/runtime-main.9c5eef44.js"
+  },
+  {
+    "revision": "184eb6ce9b94ce18c3d0bf5fb68187c3",
+    "url": "/static/media/night.184eb6ce.jpg"
+  },
+  {
+    "revision": "eba6df9ef10c4f17c7d40f1d95a33989",
+    "url": "/static/media/star-small.eba6df9e.png"
+  }
+]);
