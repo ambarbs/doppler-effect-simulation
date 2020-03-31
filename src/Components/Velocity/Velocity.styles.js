1bbca71288d0be80bec776a7265f5e92aec34ecf
Ambar Bose <ambar.bs@gmail.com>
2020-03-31 12:04:49 +1100

    Added mobile responsiveness

--- src/Components/Velocity/Velocity.styles.js
+++ src/Components/Velocity/Velocity.styles.js
@@ -6,6 +6,12 @@ export const VelocityWrapper = styled.div`
   align-items: center;
   background-color: transparent;
   min-width: ${props => props.minWidth || '250px'};
+  @media (max-width: 576px) {
+      flex-direction: column;
+      // min-height: ${props => props.minHeight};
+      min-width: unset;
+      padding-bottom: 1rem;
+  }
 `
 
 export const VelocityLabel = styled.label`
@@ -13,6 +19,9 @@ export const VelocityLabel = styled.label`
   color: white;
   font-weight: bold;
   padding-right: 0.5rem;
+   @media (max-width: 576px) {
+    padding-right: unset;
+   }
 `
 
 export const VelocityInput = styled.input`
